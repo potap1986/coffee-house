e637e95c567374b2e3be1175a3efc56544f2b10f
Киселев Михаил Викторович <kiselev_mv@mosmetro.ru>
2022-09-16 15:25:18 +0300

    Начало разметки__

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => {
   return (
     <footer className="footer container">
       <div className="footer__logo"></div>
-      <div>
-        <h2>Контакты</h2>
+      <div className="footer__contacts">
+        <h2 className="footer__title">Контакты</h2>
         <ul className="footer__nav">
           <li className="">Адрес: ул. Гагарина, 34, Рассказово, Тамбовская обл., 393250</li>
           <li className="">Режим работы: с 10 до 22, без выходных</li>
         </ul>
       </div>
       <div className="">
-        <h2>Мы в социальных сетях:</h2>
-        <ul>
+        <h2 className="footer__title">Мы в социальных сетях:</h2>
+        <ul className="footer__nav">
           <li className=""> Одноклассники </li>
           <li className=""> В контакте </li>
           <li className=""> Инстаграм </li>
